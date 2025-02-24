00b639568bf81fd3219117824cb447ef51812c37
Mahmoud Nabil <36068951+EMahmoudNabil@users.noreply.github.com>
2025-02-24 15:13:26 +0200

    attendance report

---
+++ WebApplication1/Data/Migrations/20250222220409_nulltime.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class nulltime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "TimeOut",
+                table: "Attendances",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(TimeOnly),
+                oldType: "time");
+
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "TimeIn",
+                table: "Attendances",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(TimeOnly),
+                oldType: "time");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "TimeOut",
+                table: "Attendances",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0),
+                oldClrType: typeof(TimeOnly),
+                oldType: "time",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "TimeIn",
+                table: "Attendances",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0),
+                oldClrType: typeof(TimeOnly),
+                oldType: "time",
+                oldNullable: true);
+        }
+    }
+}
---
+++ WebApplication1/ViewModels/AttendReportViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace HR.ManagmentSystem.ViewModels
+{
+    public class AttendReportViewModel
+    {
+
+        public int ID { get; set; }
+        public string EmployeeID { get; set; }
+        public string FullName { get; set; }
+        public string DepartmentName { get; set; }
+        public DateOnly Date { get; set; }
+        public TimeOnly? TimeIn { get; set; }
+        public TimeOnly? TimeOut { get; set; }
+        public string AttendanceStatus { get; set; }
+    }
+}
