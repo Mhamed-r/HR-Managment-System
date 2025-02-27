d5c3785a0cc2e0e2b65b3bf5d7ed4f310d3595a3
Mahmoud Elwesemy <mahmoudweso430@gmail.com>
2025-02-27 18:18:26 +0200

    update

---
+++ WebApplication1/ViewModels/EmployeeSalaryViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace HR.ManagmentSystem.ViewModels
+{
+    public class EmployeeSalaryViewModel
+    {
+        public string Emp_Name { get; set; }
+        public string Dep_Name { get; set; }
+        public decimal BasicSalary { get; set; }
+        public int AttendanceDays { get; set; }
+        public int AbsenceDays { get; set; }
+        public int OvertimeHours { get; set; }
+        public decimal OvertimeAmount { get; set; }
+        public int DeductionHours { get; set; }
+        public decimal DeductionAmount { get; set; }
+        public decimal TotalDeductions { get; set; }
+        public decimal NetSalary { get; set; }
+    }
+}
